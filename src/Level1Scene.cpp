f771fade4758343a118dc1bdd958f4f98e34728a
Rohan Juneja <rjuneja3@my.centennialcollege.ca>
2020-02-18 18:39:44 -0400

    Just the template provided by the professor

---
+++ src/Level1Scene.cpp
@@ -0,0 +1,131 @@
+#include "Level1Scene.h"
+#include "Game.h"
+#include <iostream>
+
+Level1Scene::Level1Scene()
+{
+	start();
+}
+
+Level1Scene::~Level1Scene()
+{
+}
+
+void Level1Scene::draw()
+{
+
+}
+
+void Level1Scene::update()
+{
+
+
+}
+
+void Level1Scene::clean()
+{
+}
+
+void Level1Scene::handleEvents()
+{
+	int wheel = 0;
+
+	SDL_Event event;
+	while(SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			TheGame::Instance()->quit();
+			break;
+		case SDL_MOUSEMOTION:
+			m_mousePosition.x = event.motion.x;
+			m_mousePosition.y = event.motion.y;
+			break;
+
+		case SDL_MOUSEBUTTONDOWN:
+			switch(event.button.button)
+			{
+			case SDL_BUTTON_LEFT:
+				
+				break;
+			}
+		
+			break;
+		case SDL_MOUSEBUTTONUP:
+			switch (event.button.button)
+			{
+			case SDL_BUTTON_LEFT:
+				
+				break;
+			}
+			break;
+		case SDL_MOUSEWHEEL:
+			wheel = event.wheel.y;
+			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				TheGame::Instance()->quit();
+				break;
+			case SDLK_1:
+				TheGame::Instance()->changeSceneState(SceneState::START_SCENE);
+				break;
+			case SDLK_2:
+				TheGame::Instance()->changeSceneState(SceneState::END_SCENE);
+				break;
+			
+
+				/************************************************************************/
+			case SDLK_w:
+				
+				break;
+			case SDLK_s:
+				
+				break;
+			case SDLK_a:
+
+				break;
+			case SDLK_d:
+
+				break;
+			}
+			
+			break;
+		case SDL_KEYUP:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_w:
+				
+				break;
+
+			case SDLK_s:
+				
+				break;
+
+			case SDLK_a:
+
+				break;
+			case SDLK_d:
+
+				break;
+			}
+			
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void Level1Scene::start()
+{
+
+}
+
+glm::vec2 Level1Scene::getMousePosition()
+{
+	return m_mousePosition;
+}
+
